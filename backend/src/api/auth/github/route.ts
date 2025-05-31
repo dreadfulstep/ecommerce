2b3a33c51d80831a5d05c208f55a8f06c1aa51df
Dreadfulstep <110997762+dreadfulstep@users.noreply.github.com>
2025-05-31 14:39:52 +0100

    Refactor: Remove backend and make frontend contents the project root

--- backend/src/api/auth/github/route.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express'
-import { ensureAuth } from '../../../middleware/ensureAuth'
-
-export const GET = ensureAuth((req: Request, res: Response) => {
-  return res.json({
-    message: 'Authenticated route',
-    user: (req as any).user,
-  })
-})
